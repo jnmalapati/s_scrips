a739d67f345a62b56d1986c2a3db13d618ff1694
Jyotheeswara Naidu Malapati <jyotheeswar.m@gmail.com>
2017-10-01 18:18:04 +0100

    init

---
+++ second-script.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a comment
+echo "Hello,	 World"
+echo "Hello World"
+echo "Hello * world"
+echo Hello * World
+echo Hello World
+echo "Hello" World
+echo "Hello" "*" "World"
+echo 'Hello' 'WOrld'
+echo 'hello' world
+echo `hello` world
